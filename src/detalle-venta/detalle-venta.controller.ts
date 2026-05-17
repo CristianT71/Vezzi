3366d98ab17f6f4a5fbb0cddbdc272cd77f6f9da
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 11:48:37 -0500

    feat(detalle-venta): adaptar tipos de id en el controlador

--- src/detalle-venta/detalle-venta.controller.ts
+++ src/detalle-venta/detalle-venta.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { DetalleVentaService } from './detalle-venta.service';
+import { CreateDetalleVentaDto } from './dto/create-detalle-venta.dto';
+import { UpdateDetalleVentaDto } from './dto/update-detalle-venta.dto';
 
 @Controller('detalle-venta')
 export class DetalleVentaController {
   constructor(private readonly detalleVentaService: DetalleVentaService) {}
 
   @Post()
-  create(@Body() body: any) {
-    return this.detalleVentaService.create();
+  create(@Body() createDto: CreateDetalleVentaDto) {
+    return this.detalleVentaService.create(createDto);
   }
 
   @Get()
@@ -21,8 +23,8 @@ export class DetalleVentaController {
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
-    return this.detalleVentaService.update(id);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateDto: UpdateDetalleVentaDto) {
+    return this.detalleVentaService.update(id, updateDto);
   }
 
   @Delete(':id')
