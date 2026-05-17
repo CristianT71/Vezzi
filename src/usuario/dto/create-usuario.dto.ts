0a508866748e2f0b60e3e91cc5ec85add6839f88
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 10:15:32 -0500

    feat(usuario): crear dto de usuario

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUsuarioDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUsuarioDto {
+
+        @IsString()
+        @IsNotEmpty()
+        @MaxLength(50)
+        nombre_usuario: string;
+
+        @IsString()
+        @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+        password: string;
+
+        @IsString()
+        @IsNotEmpty()
+        @MaxLength(155)
+        nombre_completo: string;
+
+        @IsBoolean()
+        @IsOptional()
+        activo: boolean;
+}
