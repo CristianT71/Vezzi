3831d0e2be925a390e7f49c7dfc0a235e8f4e7d2
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 11:31:43 -0500

    feat(cliente): crear entity de cliente

--- src/cliente/entities/cliente.entity.ts
+++ src/cliente/entities/cliente.entity.ts
@@ -1,1 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Cliente {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  nombre: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  telefono: string;
+
+  @Column({ type: 'decimal', precision: 12, scale: 2, default: 0 })
+  saldo_deuda: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  fecha_registro: Date;
+
+  @Column({ type: 'boolean', default: true })
+  activo: boolean;
+}
 export class Cliente {}
