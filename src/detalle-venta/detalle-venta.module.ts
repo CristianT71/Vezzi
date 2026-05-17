024086cd5a677d77732eaf014b6f76a7718ac6e1
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 11:48:00 -0500

    feat(detalle-venta): importar la entidad en el modulo usando typeorm

--- src/detalle-venta/detalle-venta.module.ts
+++ src/detalle-venta/detalle-venta.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DetalleVentaService } from './detalle-venta.service';
 import { DetalleVentaController } from './detalle-venta.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DetalleVenta } from './entities/detalle-venta.entity';
+import { Producto } from 'src/producto/entities/producto.entity';
 
 @Module({
   controllers: [DetalleVentaController],
   providers: [DetalleVentaService],
+  imports: [TypeOrmModule.forFeature([DetalleVenta, Producto])],
 })
 export class DetalleVentaModule {}
