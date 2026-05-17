f4a84746a9493a6146fa2770aa6fea38e98a00d1
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 10:30:45 -0500

    feat(rol): crear entity de rol

--- src/rol/entities/rol.entity.ts
+++ src/rol/entities/rol.entity.ts
@@ -1,1 +1,16 @@
-export class Rol {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Rol {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({type: 'varchar', length: 20})
+    nombre: string;
+
+    @Column({type: 'text', nullable: true})
+    descripcion: string;
+
+    @Column({type: 'boolean', default: true})
+    activo: boolean;
+}
