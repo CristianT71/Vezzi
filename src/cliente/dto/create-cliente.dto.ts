1aaf24bc70b4e63d177968302a13910d96dc36cf
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 11:32:11 -0500

    feat(cliente): crear dto de cliente

--- src/cliente/dto/create-cliente.dto.ts
+++ src/cliente/dto/create-cliente.dto.ts
@@ -1,1 +1,20 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsNumber, IsPhoneNumber } from 'class-validator';
+
+export class CreateClienteDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsString()
+  @IsOptional()
+  telefono: string;
+
+  @IsNumber()
+  @IsOptional()
+  saldo_deuda: number;
+
+  @IsBoolean()
+  @IsOptional()
+  activo: boolean;
+}
 export class CreateClienteDto {}
