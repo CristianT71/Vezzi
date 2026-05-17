424608be52bc3c1bddbe9871230ebb64285cbd76
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 10:32:58 -0500

    feat(rol): crear dto de rol

--- src/rol/dto/create-rol.dto.ts
+++ src/rol/dto/create-rol.dto.ts
@@ -1,1 +1,17 @@
-export class CreateRolDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreateRolDto {
+
+        @IsString()
+        @IsNotEmpty()
+        @MaxLength(20)
+        nombre: string;
+
+        @IsString()
+        @IsOptional()
+        descripcion: string;
+
+        @IsBoolean()
+        @IsOptional()
+        activo: boolean;
+}
