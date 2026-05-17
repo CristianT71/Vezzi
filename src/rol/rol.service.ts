954348d319a2b22e4e53ac7b021566e359ac8501
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-17 10:48:51 -0500

    feat(rol): implementar crud y logica de negocio en el servicio

--- src/rol/rol.service.ts
+++ src/rol/rol.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateRolDto } from './dto/create-rol.dto';
 import { UpdateRolDto } from './dto/update-rol.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Rol } from './entities/rol.entity';
 
 @Injectable()
 export class RolService {
-  create(createRolDto: CreateRolDto) {
-    return 'This action adds a new rol';
+  constructor(
+    @InjectRepository(Rol)
+    private readonly rolRepository: Repository<Rol>
+  ){}
+
+  async create(createRolDto: CreateRolDto) {
+    try {
+      const rol = this.rolRepository.create(createRolDto)
+      await this.rolRepository.save(rol)
+    } catch (error){
+      console.log(error)
+      throw new InternalServerErrorException('Error: No se puede crear el rol')
+    }
   }
 
-  findAll() {
-    return `This action returns all rol`;
+  async findAll() {
+    return this.rolRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} rol`;
+  async findOne(id: string) {
+    const rol = await this.rolRepository.findOneBy({ id })
+    if(!rol) {
+      throw new NotFoundException(`Rol con id ${id} no existe`)
+    }
+    return rol;
   }
 
-  update(id: number, updateRolDto: UpdateRolDto) {
-    return `This action updates a #${id} rol`;
+  async update(id: string, updateRolDto: UpdateRolDto) {
+    const rol = await this.rolRepository.preload({
+      id,
+      ...updateRolDto,
+    });
+    if (!rol) {
+      throw new NotFoundException(`Rol con id ${id} no existe`)
+    }
+    try {
+      await this.rolRepository.save(rol)
+      return rol;
+    } catch (error){
+      console.log(error)
+      throw new InternalServerErrorException('Error: no se pudo actualizar el rol')
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rol`;
+  async remove(id: string) {
+    const rol = await this.findOne(id)
+    await this.rolRepository.remove(rol)
+    return 'Rol eliminado con exito'
   }
 }
