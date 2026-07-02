0b2745adcb85ee90cd7ee2974903f4cab1a00fae
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-07-01 20:16:50 -0500

    feat(seguridad): agregar helmet y configurar CORS explícito

--- src/venta/venta.module.ts
+++ src/venta/venta.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Venta } from './entities/venta.entity';
 import { Cliente } from 'src/cliente/entities/cliente.entity';
 import { Usuario } from 'src/usuario/entities/usuario.entity';
+import { DetalleVenta } from 'src/detalle-venta/entities/detalle-venta.entity';
 
 @Module({
   controllers: [VentaController],
   providers: [VentaService],
-  imports: [TypeOrmModule.forFeature([Venta, Cliente, Usuario])],
+  imports: [TypeOrmModule.forFeature([Venta, Cliente, Usuario, DetalleVenta])],
 })
 export class VentaModule {}
