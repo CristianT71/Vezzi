21bf0d652c4573e66f9bf2446b21c5d74d1bbe64
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-07-01 20:28:52 -0500

    feat: agregar Global Exception Filter para respuestas de error uniformes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,6 +24,8 @@ async function bootstrap() {
     })
   )
 
-  await app.listen(process.env.PORT ?? 3000);
+  await 
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
