8ad857c62a4c3353126b8493a2672abe30bfc5e0
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-07-04 15:01:54 -0500

    feat: diseño completo del sidebar con estilos e iconos

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Sidebar],
+  imports: [Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { LucideAngularModule, LayoutDashboard, Package, Users, ShoppingCart, DollarSign, Tag, UserCheck, Shield, LogOut } from 'lucide-angular';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
