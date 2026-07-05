ade8832675f968216e9dfb6219ae0fa06608561d
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-07-05 10:16:12 -0500

    feat: agregar interceptor de token JWT

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,13 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { icons, LUCIDE_ICONS, LucideIconProvider } from 'lucide-angular';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './interceptors/token-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
     {
       provide: LUCIDE_ICONS,
       useValue: new LucideIconProvider(icons),
