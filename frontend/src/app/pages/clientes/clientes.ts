554c5a09fa9f60bcb72b9799a3a3215d8e1cd987
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-07-10 18:56:52 -0500

    feat: pagina base de clientes con listado y busqueda

--- frontend/src/app/pages/clientes/clientes.ts
+++ frontend/src/app/pages/clientes/clientes.ts
@@ -1,9 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-clientes',
-  imports: [],
+  imports: [LucideAngularModule, CommonModule, FormsModule],
   templateUrl: './clientes.html',
   styleUrl: './clientes.css',
 })
-export class Clientes {}
+export class Clientes {
+  clientes: any[] = [];
+  termino: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:3000/api/cliente?limit=50').subscribe(res => {
+      this.clientes = res.data || [];
+    });
+  }
+
+  buscar() {
+    this.http.get<any>(`http://localhost:3000/api/cliente?search=${this.termino}&limit=50`).subscribe( res => {
+      this.clientes = res.data || [];
+    });
+  }
+
+  abrirModal() {}
+}
