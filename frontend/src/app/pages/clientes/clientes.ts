6904c714f44a561783826a7fb39f1d3e575888c3
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-07-12 15:11:52 -0500

    feat: CRUD clientes con busqueda, modales crear/editar y fix navegacion

--- frontend/src/app/pages/clientes/clientes.ts
+++ frontend/src/app/pages/clientes/clientes.ts
@@ -1,32 +1,73 @@
+import { Component, OnInit, ChangeDetectorRef  } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { LucideAngularModule } from 'lucide-angular';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ClientesService } from '../../services/clientes';
 
 @Component({
   selector: 'app-clientes',
-  imports: [LucideAngularModule, CommonModule, FormsModule],
+  imports: [CommonModule, LucideAngularModule, FormsModule],
   templateUrl: './clientes.html',
   styleUrl: './clientes.css',
 })
-export class Clientes {
+export class Clientes implements OnInit {
   clientes: any[] = [];
   termino: string = '';
+  mostrarModal: boolean = false;
+  mostrarModalEditar: boolean = false;
+  nuevoCliente: any = { nombre: '', telefono: '' };
+  editarClienteData: any = {};
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private clientesService: ClientesService, 
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-  ngOnInit() {
-    this.http.get<any>('http://localhost:3000/api/cliente?limit=50').subscribe(res => {
-      this.clientes = res.data || [];
+  ngOnInit() { this.cargarClientes(); }
+
+  cargarClientes() {
+    this.clientesService.findAll(this.termino).subscribe(res => {
+    this.clientes = res.data || [];
+    this.cdr.detectChanges();
     });
   }
 
-  buscar() {
-    this.http.get<any>(`http://localhost:3000/api/cliente?search=${this.termino}&limit=50`).subscribe( res => {
-      this.clientes = res.data || [];
+  buscar() { this.cargarClientes(); }
+
+  abrirModal() {
+    this.nuevoCliente = { nombre: '', telefono: '' };
+    this.mostrarModal = true;
+    this.cdr.detectChanges();
+  }
+  cerrarModal() { 
+    this.mostrarModal = false; 
+    this.cdr.detectChanges();
+  }
+
+  crearCliente() {
+    this.clientesService.create(this.nuevoCliente).subscribe({
+      next: () => { this.cerrarModal(); this.cargarClientes(); },
+      error: (err) => { console.error(err); alert('Error al crear cliente'); },
     });
   }
 
-  abrirModal() {}
+  editarCliente(cliente: any) {
+    this.editarClienteData = { ...cliente };
+    this.mostrarModalEditar = true;
+    this.cdr.detectChanges();
+  }
+  cerrarModalEditar() { 
+    this.mostrarModalEditar = false; 
+    this.cdr.detectChanges();
+  }
+
+  actualizarCliente() {
+    const body = { nombre: this.editarClienteData.nombre, telefono: this.editarClienteData.telefono };
+    this.clientesService.update(this.editarClienteData.id, body).subscribe({
+      next: () => { this.cerrarModalEditar(); this.cargarClientes(); },
+      error: (err) => { console.error(err); alert('Error al actualizar cliente'); },
+    });
+  }
 }
