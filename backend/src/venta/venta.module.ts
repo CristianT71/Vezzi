a2a75e083f7a998d7c48641bfe71de55b12bd563
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-08-02 21:17:32 -0500

    feat: exportar ventas a Excel, devolucion con reposicion de stock y venta a credito
    Incluye fix: pago.id_venta era NOT NULL en la base de datos, impedia registrar
    abonos generales a la deuda del cliente.

--- backend/src/venta/venta.module.ts
+++ backend/src/venta/venta.module.ts
@@ -8,10 +8,12 @@ import { Venta } from './entities/venta.entity';
 import { Cliente } from 'src/cliente/entities/cliente.entity';
 import { Usuario } from 'src/usuario/entities/usuario.entity';
 import { DetalleVenta } from 'src/detalle-venta/entities/detalle-venta.entity';
+import { Producto } from 'src/producto/entities/producto.entity';
+import { HistorialStock } from 'src/historial-stock/entities/historial-stock.entity';
 
 @Module({
   controllers: [VentaController, VentaPublicoController],
   providers: [VentaService, FacturaEmailService],
-  imports: [TypeOrmModule.forFeature([Venta, Cliente, Usuario, DetalleVenta])],
+  imports: [TypeOrmModule.forFeature([Venta, Cliente, Usuario, DetalleVenta, Producto, HistorialStock])],
 })
 export class VentaModule {}
